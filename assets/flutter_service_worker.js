43a17c58ad71a51020027869145a6b66d50fbc86
Shafi <shafiurdu@gmailcom>
2022-02-01 17:18:58 +0530

    last

--- assets/flutter_service_worker.js
+++ assets/flutter_service_worker.js
@@ -66,7 +66,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "index.html": "2f1812c8a9496fc0d222139c6c2785d7",
 "/": "2f1812c8a9496fc0d222139c6c2785d7",
-"main.dart.js": "6f88233d6bc8b89aeb28e0e116b2353c",
+"main.dart.js": "36bef5a78fcd25aff9f1e59ad2ca8a64",
 "version.json": "3ef8744c286f158cc011281abc4784ad"
 };
 
